73fc00c6c954b89f8ea22dea83dcb39981bcbe56
Marco Molteni <marco.molteni@pix4d.com>
2020-11-24 10:54:30 +0100

    tests: refactor happy path

--- main_test.go
+++ main_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/pkg/diff"
 )
 
-func TestHappyPath(t *testing.T) {
+func TestSuccess(t *testing.T) {
 	testCases := []struct {
-		input      string
-		wantScript string
+		planPath       string
+		wantScriptPath string
 	}{
 		{
 			"testdata/plan-synthetic-01.txt",
@@ -20,19 +20,20 @@ func TestHappyPath(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.input, func(t *testing.T) {
-			var got bytes.Buffer
-			if err := run([]string{tc.input}, &got); err != nil {
-				t.Fatalf("got error: %v; want: no error", err)
-			}
-			want, err := ioutil.ReadFile(tc.wantScript)
+		t.Run(tc.planPath, func(t *testing.T) {
+			want, err := ioutil.ReadFile(tc.wantScriptPath)
 			if err != nil {
-				t.Fatalf("reading want file %q: error: %v", tc.wantScript, err)
+				t.Fatalf("reading want file %q: error: %v", tc.wantScriptPath, err)
+			}
+
+			var got bytes.Buffer
+			if err := run([]string{tc.planPath}, &got); err != nil {
+				t.Fatalf("got error: %v; want: no error", err)
 			}
 
 			if !bytes.Equal(got.Bytes(), want) {
 				var outDiff bytes.Buffer
-				diff.Text("got", tc.wantScript, got.Bytes(), want, &outDiff)
+				diff.Text("got", tc.wantScriptPath, got.Bytes(), want, &outDiff)
 				t.Fatalf("got the following differences:\n%v", outDiff.String())
 			}
 		})
